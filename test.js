449b0274a85b621c930b5f153c2382eba757cc8b
Eddie <i_am_uggie@yahoo.com>
2016-06-17 16:08:54 +0100

    test js page

---
+++ test.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    var x=3;
+    console.log('debug lol');
+    alert('alert box lol');
+    if(x==4) {
+
+    }
+    while (x == 4)
+    {
+        //curly bracket situation above?
+    }
+
+});
