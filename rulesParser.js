0eab20c03d50e633c1f0b73a265a110856bb14b7
eddie <i_am_uggie@yahoo.com>
2016-06-20 10:19:36 +0100

    specificity

--- rulesParser.js
+++ rulesParser.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
          {
              $('#gl-helper-tab-button').remove();
              var svgCode='<svg aria-hidden="true" class="octicon octicon-mark-github gl-svg" height="14" version="1.1" viewBox="0 0 16 16" width="14"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>';
-             $('.tabnav-pr').append('<a class="tabnav-tab js-pjax-history-navigate" id="gl-actions-enable">'+svgCode+'Enable '+appName+'</a>');
+             $('.tabnav-pr .tabnav-tabs').append('<a class="tabnav-tab js-pjax-history-navigate" id="gl-actions-enable">'+svgCode+'Enable '+appName+'</a>');
          }
     });
 
@@ -216,7 +216,7 @@ $(document).ready(function(){
         //var svgCode='<svg aria-hidden="true" class="octicon octicon-mark-github gl-svg" height="14" version="1.1" viewBox="0 0 16 16" width="14"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>';
         if(!$('.gitlint-message').length>0)
         {
-            $("<div class='gitlint-message'></div>").insertAfter('.tabnav-pr');
+            $("<div class='gitlint-message'></div>").insertAfter('.tabnav-pr .tabnav-tabs');
             $('.gitlint-message').fadeIn();
         }
         if(!quiteMode)
@@ -241,7 +241,7 @@ $(document).ready(function(){
         //var svgCode='<svg aria-hidden="true" class="octicon octicon-mark-github gl-svg" height="14" version="1.1" viewBox="0 0 16 16" width="14"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>';
         var imgURL = chrome.extension.getURL("icon48.png");
         var iconCode = '<img class="gl-icon-image small" src="'+imgURL+'" height=14 width=14 />';
-        $('.tabnav-pr').append('<a '+link+' class="tabnav-tab js-pjax-history-navigate" id="gl-helper-tab-button">'+iconCode+appName+'</a>');
+        $('.tabnav-pr .tabnav-tabs').append('<a '+link+' class="tabnav-tab js-pjax-history-navigate" id="gl-helper-tab-button">'+iconCode+appName+'</a>');
 
     }
 
@@ -257,7 +257,7 @@ function showHideUI(){
     if ($("#gl-lint-options").attr('visible')=="true")
     {
         $("#gl-lint-options").attr('visible',"false");
-        $('.tabnav-pr a').removeClass('selected');
+        $('.tabnav-pr .tabnav-tabs a').removeClass('selected');
         $tabBut.removeClass('selected');
         $('#gl-lint-options').slideUp();
     } else {
@@ -268,7 +268,7 @@ function showHideUI(){
             }
         });
         $("#gl-lint-options").attr('visible',"true");
-        $('.tabnav-pr a').removeClass('selected');
+        $('.tabnav-pr .tabnav-tabs a').removeClass('selected');
         $tabBut.addClass('selected');
         $('#gl-lint-options').slideDown();
     }
@@ -282,7 +282,7 @@ function showHideUI(){
         {
             statusClass='disabled';
         }
-        $( "<div id='gl-lint-options' class='"+statusClass+"'></div>" ).insertAfter( ".tabnav-pr" );
+        $( "<div id='gl-lint-options' class='"+statusClass+"'></div>" ).insertAfter( ".tabnav-pr .tabnav-tabs" );
         var menuTemplate = chrome.extension.getURL("menu.template.html");
         $('#gl-lint-options').load(menuTemplate);
     }
